8040bf2345226b1b24805b704b283b9f5417fa1e
edange <ewan.dange@free.fr>
2026-05-05 10:12:17 +0200

    fin premiere heure

--- Parcoursup.sql
+++ Parcoursup.sql
@@ -10,6 +10,24 @@ drop Table if EXISTS Academie;
 CREATE TABLE Academie(
     academie_nom VARCHAR(20) PRIMARY KEY;
 );
+
+
+drop TABLE if EXISTS Etablissement;
+CREATE TABLE Etablissement(
+    etablissement_code_uai VARCHAR(20) PRIMARY KEY,
+    etablissement_nom VARCHAR(20) NOT NULL,
+    etablissement_statut VARCHAR(50) Not NULL;
+);
+
+
+drop TABLE if EXISTS Filiere;
+CREATE TABLE Filiere(
+    filiere_id INT PRIMARY KEY,
+    filiere_libelle VARCHAR(20) NOT NULL,
+    filiere_libelle_tres_abrege VARCHAR(20) NOT NULL,
+    filiere_libelle_abrege VARCHAR(20) not NULL,
+    filiere_libelle_detaille_bis VARCHAR(20) NOT NULL;
+)
 ---
 
 
