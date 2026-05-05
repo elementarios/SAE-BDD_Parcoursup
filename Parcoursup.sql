5777537942da104fa985440dcd2d0d6a316d9235
TripasBibas <tripaslive@gmail.com>
2026-05-05 09:56:40 +0200

    wbimport

--- Parcoursup.sql
+++ Parcoursup.sql
@@ -6,3 +6,18 @@ set schema 'parcoursup';
 
 
 ---
+
+
+
+-- Wbimport à utiliser à la fin--
+
+/*
+Wbimport
+-mode=insert
+-file='fr-esr-parcoursup_2022.csv'
+-header=false
+-table=emp
+-schema='distribill1'
+-fileColumns=matr,nome,sal,poste,dateemb,comm,nodept
+-dateFormat='yyyy-MM-dd';
+*/
