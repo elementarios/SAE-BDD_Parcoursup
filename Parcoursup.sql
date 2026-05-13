d42048f77062116fe8167c1f7644b3b4eae5ed08
edange <ewan.dange@free.fr>
2026-05-13 14:05:45 +0200

    correction

--- Parcoursup.sql
+++ Parcoursup.sql
@@ -8,7 +8,7 @@ set schema 'parcoursup';
 ------Tables
 drop Table if EXISTS _academie;
 CREATE TABLE _academie(
-    academie_nom VARCHAR(20) PRIMARY KEY;
+    academie_nom VARCHAR(20) PRIMARY KEY
 );
 
 
@@ -16,7 +16,7 @@ drop TABLE if EXISTS _etablissement;
 CREATE TABLE _etablissement(
     etablissement_code_uai VARCHAR(20) PRIMARY KEY,
     etablissement_nom VARCHAR(20) NOT NULL,
-    etablissement_statut VARCHAR(50) Not NULL;
+    etablissement_statut VARCHAR(50) Not NULL
 );
 
 
@@ -26,13 +26,13 @@ CREATE TABLE _filiere(
     filiere_libelle VARCHAR(20) NOT NULL,
     filiere_libelle_tres_abrege VARCHAR(20) NOT NULL,
     filiere_libelle_abrege VARCHAR(20) not NULL,
-    filiere_libelle_detaille_bis VARCHAR(20) NOT NULL;
+    filiere_libelle_detaille_bis VARCHAR(20) NOT NULL
 );
 
 
 DROP TABLE if EXISTS _region;
 CREATE TABLE _region(
-    region_nom VARCHAR(20) PRIMARY KEY;
+    region_nom VARCHAR(20) PRIMARY KEY
 );
 
 
@@ -40,14 +40,14 @@ DROP TABLE if EXISTS _departement;
 CREATE TABLE _departement(
     departement_code VARCHAR(20) PRIMARY KEY,
     departement_nom VARCHAR(20),
-    region_nom VARCHAR(20) REFERENCES Region.region_nom;
+    region_nom VARCHAR(20) REFERENCES _Region(region_nom)
 );
 
 
 DROP TABLE if EXISTS _commune;
 CREATE TABLE _commune(
     commune_nom VARCHAR(20) PRIMARY KEY,
-    departement_code VARCHAR(20) REFERENCES Departement.departement_code;
+    departement_code VARCHAR(20) REFERENCES _Departement(departement_code)
 );
 
 
@@ -60,84 +60,69 @@ CREATE TABLE _formation(
     concours_communs_banque_epreuve VARCHAR(20),
     url_formation VARCHAR(20),
     tri VARCHAR(20),
-    academie_nom VARCHAR(20) REFERENCES Academie.academie_nom,
-    filiere_id INT REFERENCES Filiere.filiere_id,
-    etablissement_code_uai VARCHAR(20) REFERENCES Etablissement.etablissement_code_uai,
-    commune_nom VARCHAR(20) REFERENCES Commune.commune_nom;
+    academie_nom VARCHAR(20) REFERENCES _Academie(academie_nom),
+    filiere_id INT REFERENCES _Filiere(filiere_id),
+    etablissement_code_uai VARCHAR(20) REFERENCES _Etablissement(etablissement_code_uai),
+    commune_nom VARCHAR(20) REFERENCES _Commune(commune_nom)
 );
 
 drop table if exists _session;
-CREATE TABLE _session{
+CREATE TABLE _session(
     session_annee INT,
     primary key(session_annee)
-};
+);
 drop table if exists _regroupement;
-CREATE TABLE _regroupement{
+CREATE TABLE _regroupement(
     libelle_regroupement VARCHAR(20) NOT NULL,
     primary key(libelle_regroupement)
-};
+);
 drop table if exists _type_bac;
-CREATE TABLE _type_bac{
+CREATE TABLE _type_bac(
     type_bac VARCHAR(20) NOT NULL,
     primary key(type_bac)
-};
+);
 drop table if exists _mention_bac;
-CREATE TABLE _mention_bac{
+CREATE TABLE _mention_bac(
     libelle_mention VARCHAR(20) NOT NULL,
     primary key(libelle_mention)
-};
+);
 
 drop table if exists admissions_generalites;
-create table admissions_generalites{
+create table admissions_generalites(
     selectivite VARCHAR(20) NOT NULL,
     capacite INT,
     effectif_total_candidats INT,
     effectif_total_candidates INT,
-    cod_aff_form VARCHARv NOT NULL REFERENCES Formation(cod_aff_form),
-    session_annee INT REFERENCES Session(session_annee),
+    cod_aff_form VARCHAR NOT NULL REFERENCES _Formation(cod_aff_form),
+    session_annee INT REFERENCES _Session(session_annee),
     primary key(cod_aff_form, session_annee)
-};-- comen hair
+);-- comen hair
 
 drop table if exists admissions_selon_type_neo_bac;
-create table admissions_selon_type_neo_bac{
+create table admissions_selon_type_neo_bac(
     effectif_candidat_neo_bac_classes INT,
-    cod_aff_form VARCHAR(20) NOT NULL REFERENCES Formation(cod_aff_form),
-    session_annee INT REFERENCES Session(session_annee),
-    type_bac VARCHAR(20) NOT NULL REFERENCES Type_bac(type_bac),
+    cod_aff_form VARCHAR(20) NOT NULL REFERENCES _Formation(cod_aff_form),
+    session_annee INT REFERENCES _Session(session_annee),
+    type_bac VARCHAR(20) NOT NULL REFERENCES _Type_bac(type_bac),
     primary key(cod_aff_form, session_annee, type_bac)
-};
+);
 
 drop table if exists rang_dernier_appele_selon_regroupement;
-create table rang_dernier_appele_selon_regroupement{
-    rang_dernier_appele INT;
-    cod_aff_form VARCHAR(20) NOT NULL REFERENCES Formation(cod_aff_form),
-    libelle_regroupement VARCHARv NOT NULL REFERENCES Regroupement(libelle_regroupement),
-    session_annee INT REFERENCES Session(session_annee),
+create table rang_dernier_appele_selon_regroupement(
+    rang_dernier_appele INT,
+    cod_aff_form VARCHAR(20) NOT NULL REFERENCES _Formation(cod_aff_form),
+    libelle_regroupement VARCHAR NOT NULL REFERENCES _Regroupement(libelle_regroupement),
+    session_annee INT REFERENCES _Session(session_annee),
     primary key(cod_aff_form, libelle_regroupement, session_annee)
-};
+);
 
 drop table if exists effectif_selon_mention;
-create table effectif_selon_mention{
+create table effectif_selon_mention(
     effectif_admis_neo_bac_selon_mention INT,
-    cod_aff_form VARCHAR(20) NOT NULL REFERENCES Formation(cod_aff_form),
-    libelle_mention VARCHAR(20) NOT NULL REFERENCES Mention_bac(libelle_mention),
-    session_annee INT REFERENCES Session(session_annee),
+    cod_aff_form VARCHAR(20) NOT NULL REFERENCES _Formation(cod_aff_form),
+    libelle_mention VARCHAR(20) NOT NULL REFERENCES _Mention_bac(libelle_mention),
+    session_annee INT REFERENCES _Session(session_annee),
     primary key(cod_aff_form, libelle_mention, session_annee)
-};
-
----
+);
 
 
-
--- Wbimport à utiliser à la fin--
-
-/*
-Wbimport
--mode=insert
--file='fr-esr-parcoursup_2022.csv'
--header=false
--table=emp
--schema='distribill1'
--fileColumns=matr,nome,sal,poste,dateemb,comm,nodept
--dateFormat='yyyy-MM-dd';
-*/
